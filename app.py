cb33f9ea696cf5d9523e868718f050f5378628d4
Arham <108623726+arham-kk@users.noreply.github.com>
2023-12-20 15:32:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,75 @@
+import os
 import time
-from typing import List, Tuple, Optional
+import uuid
+from typing import List, Tuple, Optional, Dict, Union
+
 import google.generativeai as genai
 import gradio as gr
 from PIL import Image
 
-# HTML components for the UI
+print("google-generativeai:", genai.__version__)
+
+GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
+
 TITLE = """<h1 align="center">Gemini Playground 💬</h1>"""
 SUBTITLE = """<h2 align="center">Play with Gemini Pro and Gemini Pro Vision</h2>"""
 DES = """
 <div style="text-align: center; display: flex; justify-content: center; align-items: center;">
-    <span>Run with your own Google Api Key: 
+    <span>Run with your 
         <a href="https://makersuite.google.com/app/apikey">GOOGLE API KEY</a>.
     </span>
 </div>
 """
 
-# Function to preprocess stop sequences
+IMAGE_CACHE_DIRECTORY = "/tmp"
+IMAGE_WIDTH = 512
+CHAT_HISTORY = List[Tuple[Optional[Union[Tuple[str], str]], Optional[str]]]
+
 def preprocess_stop_sequences(stop_sequences: str) -> Optional[List[str]]:
-    if not stop_sequences:
-        return None
-    return [sequence.strip() for sequence in stop_sequences.split(",")]
+    return [sequence.strip() for sequence in stop_sequences.split(",")] if stop_sequences else None
 
-# Function for user interaction
-def user(text_prompt: str, chatbot: List[Tuple[str, str]]):
-    return "", chatbot + [[text_prompt, None]]
+def preprocess_image(image: Image.Image) -> Optional[Image.Image]:
+    if image:
+        image_height = int(image.height * IMAGE_WIDTH / image.width)
+        return image.resize((IMAGE_WIDTH, image_height))
 
-# Function for bot interaction
+def cache_pil_image(image: Image.Image) -> str:
+    image_filename = f"{uuid.uuid4()}.jpeg"
+    os.makedirs(IMAGE_CACHE_DIRECTORY, exist_ok=True)
+    image_path = os.path.join(IMAGE_CACHE_DIRECTORY, image_filename)
+    image.save(image_path, "JPEG")
+    return image_path
+
+def upload(files: Optional[List[str]], chatbot: CHAT_HISTORY) -> CHAT_HISTORY:
+    for file in files:
+        image = Image.open(file).convert('RGB')
+        image_preview = preprocess_image(image)
+        if image_preview:
+            # Display a preview of the uploaded image
+            gr.Image(image_preview).render()
+        image_path = cache_pil_image(image)
+        chatbot.append(((image_path,), None))
+    return chatbot
+
+def user(text_prompt: str, chatbot: CHAT_HISTORY):
+    if text_prompt:
+        chatbot.append((text_prompt, None))
+    return "", chatbot
+
 def bot(
     google_key: str,
-    image_prompt: Optional[Image.Image],
+    files: Optional[List[str]],
     temperature: float,
     max_output_tokens: int,
     stop_sequences: str,
     top_k: int,
     top_p: float,
-    chatbot: List[Tuple[str, str]]
+    chatbot: CHAT_HISTORY
 ):
-    # Check if Google API key is provided
-    if not google_key:
+    if not google_key and not GOOGLE_API_KEY:
         raise ValueError("GOOGLE_API_KEY is not set.")
 
-    # Set up Google Generative AI
-    text_prompt = chatbot[-1][0]
-    genai.configure(api_key=google_key)
+    genai.configure(api_key=google_key if google_key else GOOGLE_API_KEY)
     generation_config = genai.types.GenerationConfig(
         temperature=temperature,
         max_output_tokens=max_output_tokens,
@@ -51,20 +78,12 @@ def bot(
         top_p=top_p
     )
 
-    # Generate content based on text or image prompt
-    if image_prompt is None:
-        model = genai.GenerativeModel('gemini-pro')
-    else:
-        model = genai.GenerativeModel('gemini-pro-vision')
+    text_prompt = [chatbot[-1][0]] if chatbot and chatbot[-1][0] and isinstance(chatbot[-1][0], str) else []
+    image_prompt = [preprocess_image(Image.open(file).convert('RGB')) for file in files] if files else []
+    model_name = 'gemini-pro-vision' if files else 'gemini-pro'
+    model = genai.GenerativeModel(model_name)
+    response = model.generate_content(text_prompt + image_prompt, stream=True, generation_config=generation_config)
 
-    response = model.generate_content(
-        [text_prompt, image_prompt] if image_prompt else text_prompt,
-        stream=True,
-        generation_config=generation_config
-    )
-    response.resolve()
-
-    # Streaming effect
     chatbot[-1][1] = ""
     for chunk in response:
         for i in range(0, len(chunk.text), 10):
@@ -73,26 +92,27 @@ def bot(
             time.sleep(0.01)
             yield chatbot
 
-# Gradio components for the UI
 google_key_component = gr.Textbox(
     label="GOOGLE API KEY",
     value="",
     type="password",
     placeholder="...",
-    info="Please provide your own GOOGLE API KEY",
+    info="Please provide your own GOOGLE_API_KEY for this app",
+    visible=GOOGLE_API_KEY is None
 )
-
-image_prompt_component = gr.Image(type="pil", label="Image", scale=1)
 chatbot_component = gr.Chatbot(
     label='Gemini',
     bubble_full_width=False,
-    scale=2
+    scale=2,
+    height=600
 )
 text_prompt_component = gr.Textbox(
-    placeholder="Hi there!",
-    label="Ask me anything and press Enter"
+    placeholder="Message...", show_label=False, autofocus=True, scale=8
 )
-run_button_component = gr.Button()
+upload_button_component = gr.UploadButton(
+    label="Upload Images", file_count="multiple", file_types=["image"], scale=1
+)
+run_button_component = gr.Button(value="Run", variant="primary", scale=1)
 temperature_component = gr.Slider(
     minimum=0,
     maximum=1.0,
@@ -128,7 +148,6 @@ top_p_component = gr.Slider(
     label="Top-P",
 )
 
-# Organize UI components using Gradio Blocks
 user_inputs = [
     text_prompt_component,
     chatbot_component
@@ -136,7 +155,7 @@ user_inputs = [
 
 bot_inputs = [
     google_key_component,
-    image_prompt_component,
+    upload_button_component,
     temperature_component,
     max_output_tokens_component,
     stop_sequences_component,
@@ -145,18 +164,17 @@ bot_inputs = [
     chatbot_component
 ]
 
-# Define Gradio UI with HTML components and interaction logic
 with gr.Blocks() as demo:
     gr.HTML(TITLE)
     gr.HTML(SUBTITLE)
     gr.HTML(DES)
     with gr.Column():
         google_key_component.render()
+        chatbot_component.render()
         with gr.Row():
-            image_prompt_component.render()
-            chatbot_component.render()
-        text_prompt_component.render()
-        run_button_component.render()
+            text_prompt_component.render()
+            upload_button_component.render()
+            run_button_component.render()
         with gr.Accordion("Parameters", open=False):
             temperature_component.render()
             max_output_tokens_component.render()
@@ -165,7 +183,6 @@ with gr.Blocks() as demo:
                 top_k_component.render()
                 top_p_component.render()
 
-    # Define interaction logic for the "Run" button
     run_button_component.click(
         fn=user,
         inputs=user_inputs,
@@ -175,7 +192,6 @@ with gr.Blocks() as demo:
         fn=bot, inputs=bot_inputs, outputs=[chatbot_component],
     )
 
-    # Define interaction logic for submitting the text prompt
     text_prompt_component.submit(
         fn=user,
         inputs=user_inputs,
@@ -185,5 +201,11 @@ with gr.Blocks() as demo:
         fn=bot, inputs=bot_inputs, outputs=[chatbot_component],
     )
 
-# Launch the Gradio UI
-demo.queue(max_size=99).launch(debug=True)
+    upload_button_component.upload(
+        fn=upload,
+        inputs=[upload_button_component, chatbot_component],
+        outputs=[chatbot_component],
+        queue=False
+    )
+
+demo.queue(max_size=99).launch(debug=False, show_error=True)
